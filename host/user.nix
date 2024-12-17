977ce849854f7503f19631191cff854271b392f1
cexossqu <cexossqu@163.com>
2024-12-17 23:06:49 +0800

    add homemanger

--- host/user.nix
+++ host/user.nix
@@ -2,6 +2,7 @@
 
 let
   homeManager = inputs.home-manager.nixosModules.home-manager;
+  utilsPath = inputs.utils;
 in
 {
 
@@ -30,8 +31,8 @@ in
     useUserPackages = true;
     extraSpecialArgs = { inherit inputs; };
     users.somnium = ({config, options, lib, pkgs, ...}: let
-      utils = builtins.import (builtins.toPath (inputs.utils + "/load-modules.nix"));
-      tool = builtins.import (builtins.toPath (inputs.utils + "/tools.nix"));
+      utils = builtins.import (builtins.toPath "${utilsPath}/load-modules.nix");
+      tool = builtins.import (builtins.toPath "${utilsPath}/tools.nix"));
       # 使用加载模块功能
       allModules = utils.loadRecursiveModules ../modules;
       hmModules =  tool.filterUserNix allModules;
