c9617560505fb7646875f4790a0d490191be98cf
cexossqu <cexossqu@163.com>
2024-12-19 22:27:20 +0800

    add dockerO

---
+++ modules/docker/init.nix
@@ -0,0 +1,22 @@
+{ config, options, lib, pkgs, ... }: 
+
+{
+  # 启用docker
+  virtualisation.docker.enable = true;
+  # 用户组配置
+  users.users.somnium.extraGroups = [ "docker" ];
+  #  配置rootless
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      onlyoffice = {
+       # ...
+        image = "onlyoffice/documentserver";
+      };
+    };
+  };
+}
