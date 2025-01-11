512c8e2f2ead6bdbf0751c12b36e27018584853b
cexossqu <cexossqu@163.com>
2025-01-11 20:03:43 +0800

    change docker

--- modules/docker/init.nix
+++ modules/docker/init.nix
@@ -15,10 +15,20 @@
     containers = {
       onlyoffice = {
        # ...
-        image = "onlyoffice/documentserver";
+        image = "onlyoffice/documentserver-ee";
         ports = [
-          "127.0.0.1:8000:8000"
+          "8000:8000"
         ];
+        environment = {
+          JWT_SECRET= "my_jwt_secret";
+        };
+        volumes = [
+          "/app/onlyoffice/DocumentServer/logs:/var/log/onlyoffice"
+          "/app/onlyoffice/DocumentServer/data:/var/www/onlyoffice/Data"
+          "/app/onlyoffice/DocumentServer/lib:/var/lib/onlyoffice"
+          "/app/onlyoffice/DocumentServer/db:/var/lib/postgresql"
+        ];
+        extraOptions = "--restart always";  # 其他 Docker 选项
       };
     };
   };
